7554c1c71b081990afa1e74efa455735b426668b
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-10-24 10:02:21 +0200

    Calculate metrics on HPC

--- scripts/3_calculate_lsm.R
+++ scripts/3_calculate_lsm.R
@@ -3,46 +3,61 @@ library(dplyr)
 library(landscapemetrics)
 library(purrr)
 library(raster)
-library(rgdal)
 library(stringr)
+library(tibble)
+
+source(paste0(getwd(), "/scripts/0_calculate_lsm_helper.R"))
 
 # load the clippings
 clippings_pmm <- readRDS(paste0(getwd(), "/data/output/clippings_pmm.rds"))
 
 names_clippings <- purrr::map_chr(clippings_pmm, function(x) names(x))
+names_clippings <- stringr::str_split(names_clippings, pattern = "_", simplify = TRUE) # need for local version
 
-names_clippings <- stringr::str_split(names_clippings, pattern = "_", simplify = TRUE)
-
-# Calculate landscape-level metrics
-# metrics <- c("lsm_l_lpi", "lsm_l_area_mn", "lsm_l_split")
-
-landscape_metrics <- landscapemetrics::calculate_lsm(clippings_pmm, 
-                                                     what = "landscape")
-
+# Calculate landscape-level metrics locally
+# landscape_metrics <- landscapemetrics::calculate_lsm(clippings_pmm, 
+#                                                      what = "landscape")
+# 
 # Add name of sites
-landscape_metrics <- dplyr::mutate(landscape_metrics, 
-                                   site_a = as.integer(names_clippings[layer, 2]), 
-                                   site_b = as.integer(names_clippings[layer, 3]))
+# landscape_metrics <- dplyr::mutate(landscape_metrics, 
+#                                    site_a = as.integer(names_clippings[layer, 2]), 
+#                                    site_b = as.integer(names_clippings[layer, 3]))
+
+# Calculate landscape-level metrics on high performance cluster
+landscape_metrics <- clustermq::Q(fun = calculate_lsm_helper,
+                                  landscape = clippings_pmm, 
+                                  const = list(what = "landscape"), 
+                                  n_jobs = length(clippings_pmm),
+                                  template = list(queue = "mpi", 
+                                                  walltime = "48:00", 
+                                                  processes = 1))
+
+# Rowbind returning list and add site names
+landscape_metrics <- dplyr::bind_rows(landscape_metrics, .id = "layer_bind_rows") %>%
+  dplyr::mutate(layer = as.integer(layer_bind_rows),
+                site_a = as.integer(names_clippings[layer, 2]),
+                site_b = as.integer(names_clippings[layer, 3])) %>%
+  dplyr::select(-layer_bind_rows)
 
 # Import sample points
-sample_points <- getwd() %>% 
-  paste0("/data/SSR_17_Sites.shp") %>%
-  rgdal::readOGR() %>%
-  tibble::as.tibble()
+sample_points <- getwd() %>%
+  paste0("/data/GIS/SSR_17_sites.shp") %>%
+  raster::shapefile() %>%
+  tibble::as_tibble()
 
 # Calculate distance between each point
 distance_matrix <- sample_points %>%
-  dplyr::select(E, N) %>% # Only select the coordinates
+  dplyr::select(E, N) %>%
   dist(diag = TRUE, upper = TRUE) %>%
   as.matrix()
 
 # Add euclidean distance to each pair of sitres
-landscape_metrics <- dplyr::mutate(landscape_metrics, euclidean_distance = distance_matrix[cbind(site_a, site_b)]) %>% 
+landscape_metrics <- dplyr::mutate(landscape_metrics,
+                                   euclidean_distance = distance_matrix[cbind(site_a, site_b)]) %>% 
   dplyr::arrange(site_a, site_b)
 
 # Order and save results
-write.table(landscape_metrics, # Write to comma separeted file
+write.table(landscape_metrics_df,
             file = paste0(getwd(), '/data/output/landscape_metrics.csv'), 
             sep = ";", dec = ".",
             row.names = FALSE)
-                 
