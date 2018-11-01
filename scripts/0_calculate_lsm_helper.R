b2d85f8b28bdc82d2001947c3c05a9bda34fd49e
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-11-01 16:42:19 +0100

    Update to calculate_lsm

--- scripts/0_calculate_lsm_helper.R
+++ scripts/0_calculate_lsm_helper.R
@@ -1,7 +1,8 @@
 # Wrapper around calculate_lsm that not all options must be set for Q()
 
-calculate_lsm_helper <- function(landscape, what) {
+calculate_lsm_helper <- function(landscape, what, classes_max) {
   landscapemetrics::calculate_lsm(landscape = landscape, 
                                   what = what,
+                                  classes_max = classes_max,
                                   verbose = FALSE)
 }
--- scripts/4_calculate_lsm.R
+++ scripts/4_calculate_lsm.R
@@ -1,10 +1,10 @@
 # load libraries
-# library(clustermq)
+library(clustermq)
 library(landscapemetrics)
 library(raster)
 library(tidyverse)
 
-# source(paste0(getwd(), "/scripts/0_calculate_lsm_helper.R"))
+source(paste0(getwd(), "/scripts/0_calculate_lsm_helper.R"))
 
 # load the clippings
 clippings_pmm <- readRDS(paste0(getwd(), "/data/output/clippings_pmm.rds"))
@@ -12,47 +12,48 @@ clippings_pmm <- readRDS(paste0(getwd(), "/data/output/clippings_pmm.rds"))
 names_clippings <- purrr::map_chr(clippings_pmm, function(x) names(x))
 names_clippings <- stringr::str_split(names_clippings, pattern = "_", simplify = TRUE) # need for local version
 
-# Calculate landscape-level metrics locally
-# Nicer code but can't print overall progress at the moment...
+# # Calculate landscape-level metrics locally
+# # Nicer code but can't print overall progress at the moment...
 # landscape_metrics <- landscapemetrics::calculate_lsm(clippings_pmm,
-#                                                      what = "landscape")
+#                                                      what = c("class", "landscape"))
 # 
 # # Add name of sites
 # landscape_metrics <- dplyr::mutate(landscape_metrics,
 #                                    site_a = as.integer(names_clippings[layer, 2]),
 #                                    site_b = as.integer(names_clippings[layer, 3]))
-
-# Print progress
-landscape_metrics <- purrr::map(seq_along(clippings_pmm), function(x) {
-  
-  cat(paste0("\r> Progress: ", x, " from ", length(clippings_pmm)))
-  
-  landscapemetrics::calculate_lsm(clippings_pmm[[x]], what = c("class", "landscape"), 
-                                  verbose = FALSE)
-})
- 
-# Add name of sites
-landscape_metrics <- dplyr::bind_rows(landscape_metrics, .id = "layer_bind_rows") %>%
-  dplyr::mutate(layer = as.integer(layer_bind_rows),
-                site_a = as.integer(names_clippings[layer, 2]),
-                site_b = as.integer(names_clippings[layer, 3])) %>%
-  dplyr::select(-layer_bind_rows)
-
-# Calculate landscape-level metrics on high performance cluster
-# landscape_metrics <- clustermq::Q(fun = calculate_lsm_helper,
-#                                   landscape = clippings_pmm, 
-#                                   const = list(what = "landscape"), 
-#                                   n_jobs = length(clippings_pmm),
-#                                   template = list(queue = "mpi", 
-#                                                   walltime = "48:00", 
-#                                                   processes = 1))
 # 
-# Rowbind returning list and add site names
+# # Print progress
+# landscape_metrics <- purrr::map(seq_along(clippings_pmm), function(x) {
+# 
+#   cat(paste0("\r> Progress: ", x, " from ", length(clippings_pmm)))
+# 
+#   landscapemetrics::calculate_lsm(clippings_pmm[[x]], what = c("class", "landscape"),
+#                                   verbose = FALSE)
+# })
+# 
+# # Add name of sites
 # landscape_metrics <- dplyr::bind_rows(landscape_metrics, .id = "layer_bind_rows") %>%
 #   dplyr::mutate(layer = as.integer(layer_bind_rows),
 #                 site_a = as.integer(names_clippings[layer, 2]),
 #                 site_b = as.integer(names_clippings[layer, 3])) %>%
 #   dplyr::select(-layer_bind_rows)
+# 
+# Calculate landscape-level metrics on high performance cluster
+landscape_metrics <- clustermq::Q(fun = calculate_lsm_helper,
+                                  landscape = clippings_pmm,
+                                  const = list(what = c("class", "landscape"),
+                                               classes_max = 3),
+                                  n_jobs = length(clippings_pmm),
+                                  template = list(queue = "mpi",
+                                                  walltime = "48:00",
+                                                  processes = 1))
+
+# Rowbind returning list and add site names
+landscape_metrics <- dplyr::bind_rows(landscape_metrics, .id = "layer_bind_rows") %>%
+  dplyr::mutate(layer = as.integer(layer_bind_rows),
+                site_a = as.integer(names_clippings[layer, 2]),
+                site_b = as.integer(names_clippings[layer, 3])) %>%
+  dplyr::select(-layer_bind_rows)
 
 # Import sample points
 sample_points <- getwd() %>%
@@ -77,7 +78,7 @@ UtilityFunctions::save_rds(object = landscape_metrics,
                           path = paste0(getwd(), "/data/output"), 
                           overwrite = FALSE)
 
-write.table(landscape_metrics_df,
+write.table(landscape_metrics,
             file = paste0(getwd(), '/data/output/landscape_metrics.csv'),
             sep = ";", dec = ".",
             row.names = FALSE)
