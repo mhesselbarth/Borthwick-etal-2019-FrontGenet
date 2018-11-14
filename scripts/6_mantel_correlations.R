4f19c74fc4d4db632da894491670847698fb7527
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-11-14 14:00:09 +0100

    Update correlation/mantel

--- scripts/6_mantel_correlations.R
+++ scripts/6_mantel_correlations.R
@@ -9,14 +9,12 @@ landscape_metrics <- readr::read_rds(paste0(getwd(), "/data/output/landscape_met
 surface_metrics <- readr::read_rds(paste0(getwd(), "/data/output/surface_metrics.rds"))
 rst <- readr::read_rds(paste0(getwd(), "/data/output/rst.rds"))
 
-# only landscape level metrics and NA metrics
+# only landscape level metrics that are comparable between landscape difference size and no NA values
+absolute_metrics <- c("ca", "ndca", "np", "pafrac", "pr", "ta", "tca", "te")
 landscape_metrics <- dplyr::filter(landscape_metrics, 
                                    level == "landscape", 
-                                   !is.na(value))
-
-# add coloumn to identifiy metrics
-# landscape_metrics$metric_type <- "patch_metric"
-# surface_metrics$metric_type <- "surface_metric"
+                                   !is.na(value), 
+                                   !metric %in% absolute_metrics)
 
 #reshape surface_metrics to long format
 surface_metrics <- tidyr::gather(surface_metrics, 
@@ -36,31 +34,37 @@ surface_metrics <- left_join(x = surface_metrics,
                              by = c("site_1","site_2"), 
                              suffix = c(".surface_metrics", ".rst")) 
 
-# Calculate Mantel correlations
+# return NA in case ecodist::mantel() fails
+safe_mantel <- purrr::possibly(ecodist::mantel, otherwise = NA)
+
+# calculate Mantel correlations
 mantel_correlations_surface <- dplyr::group_by(surface_metrics, metric) %>% 
   dplyr::summarise(n = n(),
-                   mantel_cor = ecodist::mantel(RST ~ value)[[1]])
+                   mantel_cor = safe_mantel(RST ~ value)[[1]])
 
 # adding label for later plotting
 mantel_correlations_surface <- dplyr::mutate(mantel_correlations_surface, 
               label = paste0(metric, " - Mantel=", round(mantel_cor, 3)))
 
-mantel_correlations_patch <-dplyr::group_by(landscape_metrics, metric) %>% 
+# calculate Mantel correlations
+mantel_correlations_patch <- dplyr::group_by(landscape_metrics, metric) %>% 
   dplyr::summarise(n = n(),
-                   mantel_cor = ecodist::mantel(RST ~ value)[[1]])
+                   mantel_cor = safe_mantel(RST ~ value)[[1]])
 
-surface_metrics <- dplyr::left_join(x = surface_metrics, 
-                                    y =mantel_correlations_surface, 
-                                    by = "metric")
+# adding label for later plotting
+mantel_correlations_patch <- dplyr::mutate(mantel_correlations_patch, 
+                                           label = paste0(metric, " - Mantel=", round(mantel_cor, 3)))
 
-
-
-
-# mantel correlation
+# helper function to plot results
 labeller_surface <- function(variable,value){
   return(mantel_correlations_surface$label)
 }
 
+labeller_patch <- function(variable,value){
+  return(mantel_correlations_patch$label)
+}
+
+# plot results
 ggplot_correlation_rst_surface <- ggplot2::ggplot(surface_metrics, ggplot2::aes(x = value, y = RST)) + 
   ggplot2::geom_point(pch = 1) + 
   ggplot2::geom_smooth(method = "lm") +
@@ -71,6 +75,20 @@ ggplot_correlation_rst_surface <- ggplot2::ggplot(surface_metrics, ggplot2::aes(
 ggplot_correlation_rst_patch <- ggplot2::ggplot(landscape_metrics, ggplot2::aes(x = value, y = RST)) + 
   ggplot2::geom_point(pch = 1, na.rm = TRUE) + 
   ggplot2::geom_smooth(method = "lm", na.rm = TRUE) +
-  ggplot2::facet_wrap(~ metric, scales = "free_x") + 
+  ggplot2::facet_wrap(~ metric, scales = "free_x", labeller = labeller_patch) + 
   ggplot2::labs(x = "Patch metrics", y = "Rst") + 
   ggplot2::theme_bw()
+
+# save plots
+overwrite <- T
+
+
+UtilityFunctions::save_ggplot(plot = ggplot_correlation_rst_surface, 
+                              filename = "ggplot_correlation_rst_surface.png", 
+                              path = paste0(getwd(), "/plots"),
+                              overwrite = overwrite)
+
+UtilityFunctions::save_ggplot(plot = ggplot_correlation_rst_patch, 
+                              filename = "ggplot_correlation_rst_patch.png", 
+                              path = paste0(getwd(), "/plots"),
+                              overwrite = overwrite)
