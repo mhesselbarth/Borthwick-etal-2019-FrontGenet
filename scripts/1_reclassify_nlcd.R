e86ca445ba928e656cf6fa0d9d96be2441ce1986
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-11-16 10:45:11 +0100

    Reclassify NLCD

---
+++ scripts/1_reclassify_nlcd.R
@@ -0,0 +1,53 @@
+library(gridExtra)
+library(landscapetools)
+library(lattice)
+library(UtilityFunctions) # devtools::install_github("mhesselbarth/UtilityFunctions") - only needed to save data
+library(raster)
+library(rasterVis)
+library(sp)
+library(tidyverse)
+
+# Import GIS layer
+nlcd <- raster::raster(paste0(getwd(), "/data/GIS/nlcd_in.tif"))
+
+# show all unique landcover classes 
+# sort(unique(raster::values(nlcd)))
+
+# new raster to reclassify
+nlcd_reclassified <- nlcd
+
+# reclassify land-cover classes
+# forest habitat
+raster::values(nlcd_reclassified)[raster::values(nlcd_reclassified) %in% c(41, 42, 43)] <- 1
+# complementary habitat
+raster::values(nlcd_reclassified)[raster::values(nlcd_reclassified) %in% c(52, 61, 71, 81)] <- 2
+# non-habitat
+raster::values(nlcd_reclassified)[!raster::values(nlcd_reclassified) %in% c(1, 2) & 
+                                    !is.na(raster::values(nlcd_reclassified))] <- 3
+
+# convert to factor
+raster::values(nlcd) <- factor(raster::values(nlcd), 
+                               levels = c(11, 21, 22, 23, 24, 31, 41, 42, 43, 52, 71, 81, 82, 90, 95, 127), 
+                               labels = c("water", "urban", "residential (lo)", "residential (hi)", "commercial", 
+                                          "rock", "forest (d)", "forest (e)", "forest (m)", "shrub", "grass",
+                                          "pasture", "crops", "wetlands (w)", "wetlands (h)", "NA"))
+
+# convert to factor
+raster::values(nlcd_reclassified) <- factor(raster::values(nlcd_reclassified), 
+                                            levels = c(1, 2, 3), 
+                                            labels = c("forest", "complementary", "non-habitat"))
+
+
+# plot both maps
+plot_nlcd <- rasterVis::levelplot(nlcd, main = "NLCD")
+plot_nlcd_reclass <- rasterVis::levelplot(nlcd_reclassified, main = "NLCD reclassified")
+
+plot_nlcd_overall <- gridExtra::grid.arrange(plot_nlcd, 
+                                             plot_nlcd_reclass, ncol = 2)
+
+plot(plot_nlcd_overall)
+
+UtilityFunctions::save_rds(object = nlcd_reclassified, 
+                           filename = "nlcd_reclassified.rds", 
+                           path = paste0(getwd(), "/data/output"), 
+                           overwrite = FALSE)
--- scripts/2_plot_landscape.R
+++ scripts/2_plot_landscape.R
@@ -1,7 +1,7 @@
 
 # Import libraries
-library(patchwork) # devtools::install_github("thomasp85/patchwork")
-library(UtilityFunctions) # devtools::install_github("mhesselbarth/UtilityFunctions")
+library(patchwork) # devtools::install_github("thomasp85/patchwork") - arrange ggplots next to each other
+library(UtilityFunctions) # devtools::install_github("mhesselbarth/UtilityFunctions") - only needed to save data
 library(raster)
 library(sp)
 library(tidyverse)
@@ -20,31 +20,42 @@ boarder_indiana <- getwd() %>% # path to folder containing GIS data
   raster::aggregate(dissolve = T) # polygon with only the state boarder of Indiana 
 
 # Import GIS layer
-forest_300 <- getwd() %>% 
-  paste0("/data/GIS/IN_forest_300m.tif") %>%
+# forest_300 <- getwd() %>% 
+#   paste0("/data/GIS/IN_forest_300m.tif") %>%
+#   raster::raster()
+# 
+# forest_300_relative <- getwd() %>% 
+#     paste0("/data/GIS/IN_forest_2.1Wind_300m.tif") %>%
+#     raster::raster()
+# 
+# forest_cover_30 <- getwd() %>% 
+#     paste0("/data/GIS/IN_forest_cover_30m.tif") %>%
+#     raster::raster()
+# 
+# # Set NA to 0 
+# forest_300[is.na(forest_300)] <- 0 # Set NA values to 0, because there are NAs inside the state
+# forest_300 <- raster::mask(x = forest_300, mask = boarder_indiana) # clip/mask data only to the state boarder
+
+# Import GIS layer
+nlcd <- getwd() %>%
+  paste0("/data/GIS/nlcd_in.tif") %>%
   raster::raster()
 
-forest_300_relative <- getwd() %>% 
-    paste0("/data/GIS/IN_forest_2.1Wind_300m.tif") %>%
-    raster::raster()
-
-forest_cover_30 <- getwd() %>% 
-    paste0("/data/GIS/IN_forest_cover_30m.tif") %>%
-    raster::raster()
-
-# Set NA to 0 
-forest_300[is.na(forest_300)] <- 0 # Set NA values to 0, because there are NAs inside the state
-forest_300 <- raster::mask(x = forest_300, mask = boarder_indiana) # clip/mask data only to the state boarder
+# convert raster to dataframe with xy-coordinates and z-values
+nlcd_df <- raster::as.data.frame(nlcd_df, 
+                                 na.rm = TRUE, xy = TRUE)
+  
 
 # Import habitat surface
 habitat_surface <- getwd() %>% 
   paste0("/data/GIS/habitat_surface.tif") %>%
   raster::raster()
 
+# convert raster to dataframe with xy-coordinates and z-values
+habitat_surface_df <- raster::as.data.frame(habitat_surface, 
+                                            na.rm = TRUE, xy = TRUE)
 # Plot gis layer with sampling points
-ggplot_forest <- forest_300 %>%
-  raster::as.data.frame(na.rm = TRUE, xy = TRUE) %>% # convert raster to dataframe with xy-coordinates and z-values
-  ggplot2::ggplot() + # plot data
+ggplot_nlcd_df <- ggplot2::ggplot(data = nlcd_df) + # plot data
     ggplot2::geom_raster(aes(x = x, y = y, fill = factor(IN_forest_300m))) + # plot patches with fill according to value
     ggplot2::geom_point(data = tibble::as.tibble(sampling_points), # plot sampling points
                         aes(x = x, y = y, col = "red"), shape = 19, size = 2) +
@@ -56,9 +67,7 @@ ggplot_forest <- forest_300 %>%
     ggplot2::theme(axis.text=element_blank(), # remove xy-coordinates from axis (not very informative)
                    axis.ticks=element_blank())
 
-ggplot_habitat_surface <- habitat_surface %>%
-  raster::as.data.frame(na.rm = TRUE, xy = TRUE) %>% # convert raster to dataframe with xy-coordinates and z-values
-  ggplot2::ggplot() + # plot data
+ggplot_habitat_surface <- ggplot2::ggplot(data = habitat_surface_df) + # plot data
   ggplot2::geom_raster(aes(x = x, y = y, fill = habitat_surface)) + # plot patches with fill according to value
   ggplot2::geom_point(data = tibble::as.tibble(sampling_points), # plot sampling points
                       aes(x = x, y = y, col = "red"), shape = 19, size = 2) +
