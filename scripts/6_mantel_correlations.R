67207c737df71935b42358c70f1f37463bd34860
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-01-10 09:41:06 +0100

    Adding mantel correlations forest class

--- scripts/6_mantel_correlations.R
+++ scripts/6_mantel_correlations.R
@@ -8,36 +8,40 @@ landscape_metrics <- readr::read_rds(paste0(getwd(), "/data/output/landscape_met
 surface_metrics <- readr::read_rds(paste0(getwd(), "/data/output/surface_metrics.rds"))
 rst <- readr::read_rds(paste0(getwd(), "/data/output/rst.rds"))
 
-# only landscape level metrics that are comparable between landscape difference size and no NA values
-# absolute_metrics <- c("ca", "ndca", "np", "pafrac", "pr", "ta", "tca", "te")
-
+# filter data 
 landscape_level <- dplyr::filter(landscape_metrics,
                                  level == "landscape",
                                  !is.na(value))
 
-# class_level_forest <- dplyr::filter(landscape_metrics,
-#                                     level == "class",
-#                                     class == 1,
-#                                     !is.na(value))
-# 
-# class_level_complementary <- dplyr::filter(landscape_metrics,
-#                                            level == "class",
-#                                            class == 2,
-#                                            !is.na(value))
+class_forest <- dplyr::filter(landscape_metrics,
+                              level == "class",
+                              class == 1,
+                              !is.na(value))
 
+# class_complementary <- dplyr::filter(landscape_metrics,
+#                                      level == "class",
+#                                      class == 2,
+#                                      !is.na(value))
 
-#reshape surface_metrics to long format
+# reshape surface_metrics to long format
 surface_metrics <- tidyr::gather(surface_metrics, 
                                  -name, -landscape, -site_1, -site_2,
                                  key = "metric", value = "value")
-
 
 # adding RST
 landscape_level <- left_join(x = landscape_level, 
                              y = rst,
                              by = c("site_a" = "site_1", 
-                                    "site_b" = "site_2"),
-                             suffix = c(".surface_metrics", ".rst"))
+                                    "site_b" = "site_2"))
+
+class_forest <- left_join(x = class_forest, 
+                          y = rst,
+                          by = c("site_a" = "site_1", 
+                                 "site_b" = "site_2"))
+
+# class_complementary <- left_join(x = class_complementary, 
+#                                  y = rst,
+#                                  by = c("site_a" = "site_1"))
 
 surface_metrics <- left_join(x = surface_metrics, 
                              y = rst,
@@ -47,7 +51,7 @@ surface_metrics <- left_join(x = surface_metrics,
 # return NA in case ecodist::mantel() fails
 safe_mantel <- purrr::possibly(ecodist::mantel, otherwise = NA)
 
-# calculate Mantel correlations
+# calculate Mantel correlations surface metrics
 mantel_correlations_surface <- dplyr::group_by(surface_metrics, metric) %>% 
   dplyr::summarise(n = n(),
                    mantel_cor = safe_mantel(RST ~ value)[[1]])
@@ -56,38 +60,80 @@ mantel_correlations_surface <- dplyr::group_by(surface_metrics, metric) %>%
 mantel_correlations_surface <- dplyr::mutate(mantel_correlations_surface, 
               label = paste0(metric, " - Mantel=", round(mantel_cor, 3)))
 
-# calculate Mantel correlations
-mantel_correlations_patch <- dplyr::group_by(landscape_level, metric) %>% 
+# calculate Mantel correlations landscape level
+mantel_correlations_landscape_level <- dplyr::group_by(landscape_level, metric) %>% 
   dplyr::summarise(n = n(),
                    mantel_cor = safe_mantel(RST ~ value)[[1]])
 
 # adding label for later plotting
-mantel_correlations_patch <- dplyr::mutate(mantel_correlations_patch, 
-                                           label = paste0(metric, " - Mantel=", round(mantel_cor, 3)))
+mantel_correlations_landscape_level <- dplyr::mutate(mantel_correlations_landscape_level, 
+                                                     label = paste0(metric, " - Mantel=", round(mantel_cor, 3)))
+
+# calculate Mantel correlations forest class
+mantel_correlations_forest <- dplyr::group_by(class_forest, metric) %>% 
+  dplyr::summarise(n = n(),
+                   mantel_cor = safe_mantel(RST ~ value)[[1]])
+
+# adding label for later plotting
+mantel_correlations_forest <- dplyr::mutate(mantel_correlations_forest, 
+                                            label = paste0(metric, " - Mantel=", round(mantel_cor, 3)))
+
+# # calculate Mantel correlations complementary class
+# mantel_correlations_complementary <- dplyr::group_by(class_complementary, metric) %>% 
+#   dplyr::summarise(n = n(),
+#                    mantel_cor = safe_mantel(RST ~ value)[[1]])
+# 
+# # adding label for later plotting
+# mantel_correlations_complementary <- dplyr::mutate(mantel_correlations_complementary, 
+#                                                    label = paste0(metric, " - Mantel=", round(mantel_cor, 3)))
+
+
 
 # helper function to plot results
-labeller_surface <- function(variable,value){
+labeller_surface <- function(variable, value){
   return(mantel_correlations_surface$label)
 }
 
-labeller_patch <- function(variable,value){
-  return(mantel_correlations_patch$label)
+labeller_landscape_level <- function(variable, value){
+  return(mantel_correlations_landscape_level$label)
 }
+
+labeller_class_forest <- function(variable, value){
+  return(mantel_correlations_forest$label)
+}
+
+# labeller_class_complementary <- function(variable, value){
+#   return(mantel_correlations_complementary$label)
+# }
 
 # plot results
 ggplot_correlation_rst_surface <- ggplot2::ggplot(surface_metrics, ggplot2::aes(x = value, y = RST)) + 
   ggplot2::geom_point(pch = 1) + 
   ggplot2::geom_smooth(method = "lm") +
   ggplot2::facet_wrap(~ metric, scales = "free_x", labeller = labeller_surface) + 
-  ggplot2::labs(x = "Surface metrics", y = "Rst") + 
+  ggplot2::labs(x = "Surface metrics", y = "Rst", title = "Surface metrics") + 
   ggplot2::theme_bw()
 
-ggplot_correlation_rst_patch <- ggplot2::ggplot(landscape_level, ggplot2::aes(x = value, y = RST)) + 
+ggplot_correlation_rst_landscape_level <- ggplot2::ggplot(landscape_level, ggplot2::aes(x = value, y = RST)) + 
   ggplot2::geom_point(pch = 1, na.rm = TRUE) + 
   ggplot2::geom_smooth(method = "lm", na.rm = TRUE) +
-  ggplot2::facet_wrap(~ metric, scales = "free_x", labeller = labeller_patch) + 
-  ggplot2::labs(x = "Patch metrics", y = "Rst") + 
+  ggplot2::facet_wrap(~ metric, scales = "free_x", labeller = labeller_landscape_level) + 
+  ggplot2::labs(x = "Patch metrics", y = "Rst", title = "Landscape level") + 
   ggplot2::theme_bw()
+
+ggplot_correlation_rst_class_forest <- ggplot2::ggplot(class_forest, ggplot2::aes(x = value, y = RST)) + 
+  ggplot2::geom_point(pch = 1, na.rm = TRUE) + 
+  ggplot2::geom_smooth(method = "lm", na.rm = TRUE) +
+  ggplot2::facet_wrap(~ metric, scales = "free_x", labeller = labeller_class_forest) + 
+  ggplot2::labs(x = "Patch metrics", y = "Rst", title = "Class: Forest") + 
+  ggplot2::theme_bw()
+
+# ggplot_correlation_rst_class_complementary <- ggplot2::ggplot(class_forest, ggplot2::aes(x = value, y = RST)) + 
+#   ggplot2::geom_point(pch = 1, na.rm = TRUE) + 
+#   ggplot2::geom_smooth(method = "lm", na.rm = TRUE) +
+#   ggplot2::facet_wrap(~ metric, scales = "free_x", labeller = labeller_class_complementary) + 
+#   ggplot2::labs(x = "Patch metrics", y = "Rst", title = "Class: Complementary") + 
+#   ggplot2::theme_bw()
 
 # save plots
 overwrite <- FALSE
@@ -97,7 +143,18 @@ UtilityFunctions::save_ggplot(plot = ggplot_correlation_rst_surface,
                               path = paste0(getwd(), "/plots"),
                               overwrite = overwrite)
 
-UtilityFunctions::save_ggplot(plot = ggplot_correlation_rst_patch, 
-                              filename = "ggplot_correlation_rst_patch.png", 
+UtilityFunctions::save_ggplot(plot = ggplot_correlation_rst_landscape_level, 
+                              filename = "ggplot_correlation_rst_landscape_level.png", 
                               path = paste0(getwd(), "/plots"),
                               overwrite = overwrite)
+
+UtilityFunctions::save_ggplot(plot = ggplot_correlation_rst_class_forest, 
+                              filename = "ggplot_correlation_rst_class_forest.png", 
+                              path = paste0(getwd(), "/plots"),
+                              overwrite = overwrite)
+
+# UtilityFunctions::save_ggplot(plot = ggplot_correlation_rst_class_complementary, 
+#                               filename = "ggplot_correlation_rst_class_complementary.png", 
+#                               path = paste0(getwd(), "/plots"),
+#                               overwrite = overwrite)
+
