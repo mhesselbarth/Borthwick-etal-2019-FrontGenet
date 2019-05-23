843f0f7fe6c772d150788b8f89776eab7946015d
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-05-23 15:23:46 +0200

    Small updates future HPC

--- scripts/4_calculate_lsm.R
+++ scripts/4_calculate_lsm.R
@@ -22,59 +22,38 @@ names_clippings <- purrr::map_chr(clippings_pmm, function(x) names(x))
 
 names_clippings <- stringr::str_split(names_clippings, pattern = "_", simplify = TRUE) # need for local version
 
-# class <- c("lsm_c_ai", 
-#            "lsm_c_area_mn", 
-#            "lsm_c_ca", 
-#            "lsm_c_cai_mn", 
-#            "lsm_c_clumpy", 
-#            "lsm_c_core_mn", 
-#            "lsm_c_cpland", 
-#            "lsm_c_division", 
-#            "lsm_c_iji", 
-#            "lsm_c_lpi", 
-#            "lsm_c_lsi",
-#            "lsm_c_mesh", 
-#            "lsm_c_np", 
-#            "lsm_c_pd", 
-#            "lsm_c_pladj", 
-#            "lsm_c_pland", 
-#            "lsm_c_split", 
-#            "lsm_c_te")
-
-landscape <- c("lsm_l_ai", 
-               "lsm_l_area_mn", 
-               "lsm_l_cai_mn", 
-               "lsm_l_condent", 
-               "lsm_l_contag", 
-               "lsm_l_core_mn", 
-               "lsm_l_division", 
-               "lsm_l_ed", 
-               "lsm_l_ent", 
-               "lsm_l_iji", 
-               "lsm_l_joinent",
-               "lsm_l_lpi", 
-               "lsm_l_lsi", 
-               "lsm_l_mesh", 
-               "lsm_l_mutinf", 
-               "lsm_l_np", 
-               "lsm_l_pd", 
-               "lsm_l_pladj", 
-               "lsm_l_pr", 
-               "lsm_l_prd", 
-               "lsm_l_rpr", 
-               "lsm_l_shdi", 
-               "lsm_l_shei",
-               "lsm_l_sidi,",
-               "lsm_l_siei",
-               "lsm_l_split", 
-               "lsm_l_ta", 
-               "lsm_l_te")
-
-# what <- c(class, landscape)
+landscape_sub <- c("lsm_l_ai", 
+                   "lsm_l_area_mn", 
+                   "lsm_l_cai_mn", 
+                   "lsm_l_condent", 
+                   "lsm_l_contag", 
+                   "lsm_l_core_mn", 
+                   "lsm_l_division", 
+                   "lsm_l_ed", 
+                   "lsm_l_ent", 
+                   "lsm_l_iji", 
+                   "lsm_l_joinent",
+                   "lsm_l_lpi", 
+                   "lsm_l_lsi", 
+                   "lsm_l_mesh", 
+                   "lsm_l_mutinf", 
+                   "lsm_l_np", 
+                   "lsm_l_pd", 
+                   "lsm_l_pladj", 
+                   "lsm_l_pr", 
+                   "lsm_l_prd", 
+                   "lsm_l_rpr", 
+                   "lsm_l_shdi", 
+                   "lsm_l_shei",
+                   "lsm_l_sidi,",
+                   "lsm_l_siei",
+                   "lsm_l_split", 
+                   "lsm_l_ta", 
+                   "lsm_l_te")
 
 # # Calculate metrics locally
 # landscape_metrics <- landscapemetrics::calculate_lsm(clippings_pmm,
-#                                                      what = landscape,
+#                                                      what = landscape_sub,
 #                                                      classes_max = 3)
 # 
 # # Calculate metrics locally but overall printing progress
@@ -85,7 +64,7 @@ landscape <- c("lsm_l_ai",
 #   print(paste0("Progress: ", x, " from ", total_clippigings))
 # 
 #   result <- calculate_lsm(landscape = clippings_pmm[[x]],
-#                           what = landscape,
+#                           what = landscape_sub,
 #                           classes_max = 3,
 #                           verbose = FALSE,
 #                           progress = FALSE)
@@ -98,7 +77,7 @@ landscape <- c("lsm_l_ai",
 # Calculate metrics on high performance cluster
 landscape_metrics <- clustermq::Q(fun = calculate_lsm_helper,
                                   landscape = clippings_pmm,
-                                  const = list(what = landscape,
+                                  const = list(what = landscape_sub,
                                                classes_max = 3),
                                   n_jobs = length(clippings_pmm),
                                   template = list(queue = "fat",
@@ -131,7 +110,8 @@ distance_matrix <- sample_points %>%
 
 # Add euclidean distance to each pair of sitres
 landscape_metrics <- dplyr::mutate(landscape_metrics,
-                                   euclidean_distance = distance_matrix[cbind(site_a, site_b)]) %>% 
+                                   euclidean_distance = distance_matrix[cbind(site_a, 
+                                                                              site_b)]) %>% 
   dplyr::arrange(site_a, site_b)
 
 # Order and save results
@@ -153,28 +133,32 @@ write.table(landscape_metrics,
 # 
 # # now we specify a future topology that fits our HPC
 # # login node -> cluster nodes -> core/ multiple cores
-# login <- tweak(future::remote, 
-#                workers = "gwdu103.gwdg.de", 
-#                user = 'hesselbarth3') # user = login credential
+# login <- tweak(future::remote,
+#                workers = "gwdu103.gwdg.de",
+#                user = "hesselbarth3") # user = login credential
 # 
-# sbatch <- tweak(future.batchtools::batchtools_slurm, 
-#                 template = 'future_slurm.tmpl',
-#                 resources = list(job.name = 'calculate_lsm', # name of the job
-#                                  log.file = 'calculate_lsm.log', # name of log file
-#                                  queue = 'fat', # which partition
-#                                  service = "medium", # which QOS
-#                                  walltime = '06:00:00', # walltime <hh:mm:ss>
+# sbatch <- tweak(future.batchtools::batchtools_slurm,
+#                 template = "future_slurm.tmpl",
+#                 resources = list(job.name = "calculate_lsm", # name of the job
+#                                  log.file = "calculate_lsm.log", # name of log file
+#                                  queue = "fat", # which partition
+#                                  service = "normal", # which QOS
+#                                  walltime = "01:00:00", # walltime <hh:mm:ss>
 #                                  processes = 1)) # number of cores
-#  
+# 
 # future::plan(list(login, sbatch, future::sequential)) # how to run on nodes, could also be sequential
+# 
+# # no max size of globals
+# options(future.globals.maxSize = Inf)
 # 
 # landscape_metrics %<-% furrr::future_map(clippings_pmm, function(x) {
 # 
 #   calculate_lsm(landscape = x,
-#                 what = what,
+#                 what = landscape_sub,
 #                 classes_max = 3,
 #                 verbose = FALSE,
 #                 progress = FALSE)
 # })
 # 
 # future::resolved(future::futureOf(landscape_metrics))
+
